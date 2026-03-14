dcd75b46b824f78aa62ce85c2e14c41e08262e2d
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-03-14 21:55:11 +0530

    Add files via upload

---
+++ basicstrings.java
@@ -0,0 +1,121 @@
+package Stringsyoutube;
+
+//
+//    public class basicstrings {
+//        public boolean palindrome(String s) {
+//            int i = 0;
+//            int j = s.length() - 1;
+//            while (i <= j) {
+//                if (s.charAt(i) != s.charAt(j)) {
+//                    return false;
+//                }
+//                i++;
+//                j--;
+//            }
+//            return true;
+//        }
+//
+//
+//        public static void main(String[] args) {
+//            String s = "madams";
+////            basicstrings p = new basicstrings();
+////            boolean result;
+////            result = p.palindrome(s);
+//            System.out.println(s.startsWith("d"));
+//        }
+//    }
+
+
+
+
+//public class basicstrings {
+//    public static int  comparestring(String a , String b){
+//        for (int i=0;i<a.length();i++){
+//            for (int j=0; j<b.length();j++){
+//                if(a!=b){
+//                    return 1;
+//                }
+//            }
+//        }
+//        return -1;
+//    }
+//
+//    public static void main(String[] args) {
+//        String a= "pawani";
+//        String b="Shriyansh";
+//        System.out.println(comparestring(a,b));}
+//}
+
+
+
+//public class basicstrings {
+//    public static void main(String[] args) {
+//        String s= "pAWANI";
+//
+//        if (Character.isUpperCase(s.charAt(0))){
+//            s = s.toUpperCase();
+//        }
+//        else {
+//            s= s.toLowerCase();
+//        }
+//        System.out.println("result: " + s);
+//    }
+//}
+
+
+
+
+//
+//public class basicstrings {
+//    public static void main(String[] args) {
+//        String str ="pawani";
+//        for (int i = 0; i < str.length(); i++) {
+//            for (int j = 1; j <= str.length(); j++) {
+//                System.out.println(str.substring(i,j));
+//            }
+//        }
+//    }
+//}
+
+
+
+/// //////// to find substrings of the string ////////////////////////
+
+//public class LargestString {
+//    public static void main(String[] args) {
+//        String str ="pawani";
+//        for (int i = 0; i < str.length(); i++) {
+//            for (int j = i; j < str.length(); j++) {
+//                System.out.print(str.substring(i,j+1) + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
